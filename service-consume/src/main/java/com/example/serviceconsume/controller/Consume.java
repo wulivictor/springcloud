c5ca5b0f772e899e286a31a32bc0c01dca8b11ae
Administrator <1351698648@qq.com>
2018-12-19 16:06:28 +0800

    服务注册

---
+++ service-consume/src/main/java/com/example/serviceconsume/controller/Consume.java
@@ -0,0 +1,34 @@
+package com.example.serviceconsume.controller;
+
+import com.example.serviceconsume.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+/**
+ * @project; springcloud
+ * @description:
+ * @author: wuli
+ * @create: 2018-12-19 11:02
+ */
+@RestController
+public class Consume {
+
+    @Autowired
+    private  RestTemplate restTemplate;
+
+    @Value("${user.servicePath}")
+    private String servicePath;
+
+    @GetMapping("/consume/{id}")
+    public ResponseEntity<User> findbyId(@PathVariable Long id ){
+        return restTemplate.getForEntity(this.servicePath + id, User.class);
+    }
+
+}
---
+++ service-consume/src/test/java/com/example/serviceconsume/ServiceConsumeApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.serviceconsume;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceConsumeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
