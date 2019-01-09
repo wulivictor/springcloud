c179d5e02dac7a2568a6715a7402e48a571b2d79
Administrator <1351698648@qq.com>
2019-01-09 11:57:54 +0800

    开启hystrix仪表盘

--- ribbon-with-hystrix/src/main/java/com/example/hystrix/HystrixApplication.java
+++ ribbon-with-hystrix/src/main/java/com/example/hystrix/HystrixApplication.java
@@ -1,10 +1,14 @@
 package com.example.hystrix;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -12,14 +16,29 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard // 开启hystrix仪表盘
 public class HystrixApplication {
 
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
-
     }
+
+    // 开启hystrix 仪表盘
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);  //系统启动时加载顺序
+        registrationBean.addUrlMappings("/hystrix.stream");//路径
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
 
     public static void main(String[] args) {
 
--- ribbon-with-hystrix/src/main/java/com/example/hystrix/controller/HystrixController.java
+++ ribbon-with-hystrix/src/main/java/com/example/hystrix/controller/HystrixController.java
@@ -2,12 +2,8 @@ package com.example.hystrix.controller;
 
 import com.example.hystrix.entity.User;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.cloud.netflix.hystrix.HystrixCommands;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +27,7 @@ public class HystrixController {
     @GetMapping(value = "movie/{id}")
     @HystrixCommand(fallbackMethod = "findByIdCallBack")
     public User findUserById(@PathVariable Long id) {
+        System.out.println("》》》消费者");
 
         return restTemplate.getForObject("http://SERVICE-PROVIDER/simple/" + id, User.class);
     }
@@ -43,6 +40,12 @@ public class HystrixController {
     }
 
 
+/**
+    THREAD(线程隔离）：使用该方式，HystrixCommand将会在单独的线程上执行，并发请求受线程池中线程数量的限制。
+            SEMAPHORE（信号量隔离）：使用该方式，HystrixCommand将会在调用线程上执行，开销相对较小，并发请求受到信号量个数的限制。
+            Hystrix中默认并且推荐使用线程隔离（THREAD)，因为这种方式有一个除网络超时以外的额外保护。
+   一般来说，只有当调用负载异常高时（例如每个实例每秒调用数百次）才需要信号量隔离，因为这种场景下使用THREAD开销会比较高。信号量隔离一般仅适用于非网络调用的隔离。
+**/
 
 
 
