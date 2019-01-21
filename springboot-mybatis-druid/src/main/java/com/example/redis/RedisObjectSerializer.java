ee2409e0f8eb783aaa79cabfdd52d8b271b7f8e3
Administrator <1351698648@qq.com>
2019-01-21 10:57:36 +0800

    普通模式下的抢购 和redis缓存下的抢购

--- springboot-mybatis-druid/src/main/java/com/example/redis/RedisObjectSerializer.java
+++ springboot-mybatis-druid/src/main/java/com/example/redis/RedisObjectSerializer.java
@@ -5,8 +5,11 @@ import org.springframework.core.serializer.support.DeserializingConverter;
 import org.springframework.core.serializer.support.SerializingConverter;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
+
+
 // 此时定义的序列化操作表示可以序列化所有类的对象，当然，这个对象所在的类一定要实现序列化接口
 public class RedisObjectSerializer implements RedisSerializer<Object> {
+
     // 为了方便进行对象与字节数组的转换，所以应该首先准备出两个转换器
     private Converter<Object, byte[]> serializingConverter = new SerializingConverter();
     private Converter<byte[], Object> deserializingConverter = new DeserializingConverter();
